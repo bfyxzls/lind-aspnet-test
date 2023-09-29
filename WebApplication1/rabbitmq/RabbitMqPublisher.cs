86d150aedf7e028766c4a8473a1802c437eaad70
lind <bfyxzls@sina.com>
2023-09-29 20:17:52 +0800

    添加WEB

---
+++ WebApplication1/rabbitmq/RabbitMqPublisher.cs
@@ -0,0 +1,181 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace WEBAPI.Core
+{
+    /// <summary>
+    /// RabbitMq消息生产者
+    /// </summary>
+    public class RabbitMQPublisher
+    {
+        private string _uri;
+        /// <summary>
+        /// 交换器
+        /// </summary>
+        private string exchangeName = "";
+        private readonly IConnection connection;
+        //private readonly IModel channel;
+
+        /// <summary>
+        /// 初始化
+        /// 子类去实现相关的rabbit地址,端口和授权
+        /// </summary>
+        /// <param name="uri">消息服务器地址</param>
+        /// <param name="userName">用户名</param>
+        /// <param name="password">密码</param>
+        /// <param name="exchangeName">交换器,为空表示分发模式,否则为广播模式</param>
+        public RabbitMQPublisher(string uri = "amqp://localhost:5672", string userName = "guest", string password = "guest", string exchangeName = "")
+        {
+            _uri = uri;
+            var factory = new ConnectionFactory() { Uri = new Uri(_uri) };
+            if (!string.IsNullOrWhiteSpace(exchangeName))
+                this.exchangeName = exchangeName;
+            if (!string.IsNullOrWhiteSpace(userName))
+                factory.UserName = userName;
+            if (!string.IsNullOrWhiteSpace(userName))
+                factory.Password = password;
+            connection = factory.CreateConnection();
+            //this.channel = connection.CreateModel();
+        }
+
+        /// <summary>
+        /// 将消息推送到服务器
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        public void Publish<TMessage>(string queue, TMessage message)
+        {
+            Publish(null, queue, message);
+        }
+
+        /// <summary>
+        /// 将消息推送到服务器
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="queue"></param>
+        /// <param name="message"></param>
+        /// <param name="durable">是否持久化</param>
+        /// <param name="arguments">消息优先级0-10（越大越高）</param>
+        public void Publish<TMessage>(string exchange, string queue, TMessage message)
+        {
+            if (!string.IsNullOrWhiteSpace(exchange))
+                exchangeName = exchange;
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queue,//队列名
+                                           durable: true,//是否持久化
+                                           exclusive: false,//排它性
+                                           autoDelete: false,//一旦客户端连接断开则自动删除queue
+                                           arguments: null);//如果安装了队列优先级插件则可以设置优先级
+
+                var json = JsonHelper.SerializeObject(message);
+                var bytes = Encoding.UTF8.GetBytes(json);
+                Console.WriteLine("向服务器{0}推消息", _uri);
+                channel.BasicPublish(exchange: this.exchangeName, routingKey: queue, basicProperties: null, body: bytes);
+            }
+        }
+
+        public void PublishFanout<TMessage>(TMessage message)
+        {
+            PublishFanout(null, message);
+        }
+
+        /// <summary>
+        /// 广播消息,需要在初始化时为exchangeName赋值
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="message"></param>
+        public void PublishFanout<TMessage>(string exchange, TMessage message)
+        {
+            if (!string.IsNullOrWhiteSpace(exchange))
+                exchangeName = exchange;
+            const string ROUTING_KEY = "";
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(this.exchangeName, "fanout");//广播
+                var json = JsonHelper.SerializeObject(message);
+                var bytes = Encoding.UTF8.GetBytes(json);
+                channel.BasicPublish(this.exchangeName, ROUTING_KEY, null, bytes);//不需要指定routing key，设置了fanout,指了也没有用.
+                Console.WriteLine(DateTime.Now + " 向服务器{0}推消息", _uri);
+            }
+        }
+        /// <summary>
+        /// 批量将消息推送到服务器
+        /// </summary>
+        public void PublishBatch<TMessage>(string queue, List<TMessage> messages)
+        {
+            PublishBatch(null, queue, messages);
+        }
+        /// <summary>
+        /// 批量将消息推送到服务器
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="exchange"></param>
+        /// <param name="queue"></param>
+        /// <param name="messages"></param>
+        /// <param name="durable">是否持久化</param>
+        /// <param name="arguments">消息优先级0-10（越大越高）</param>
+        public void PublishBatch<TMessage>(string exchange, string queue, List<TMessage> messages)
+        {
+            if (!string.IsNullOrWhiteSpace(exchange))
+                exchangeName = exchange;
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                queue: queue, //队列名
+                durable: true, //是否持久化
+                exclusive: false, //排它性
+                autoDelete: false,//一旦客户端连接断开则自动删除queue
+                arguments: null);//如果安装了队列优先级插件则可以设置优先级
+
+                var batch = channel.CreateBasicPublishBatch();
+
+                foreach (var message in messages)
+                {
+                    try
+                    {
+                        var json = JsonHelper.SerializeObject(message);
+                        var body = Encoding.UTF8.GetBytes(json);
+                        batch.Add(exchange: this.exchangeName, routingKey: queue, mandatory: false, properties: null, body: body);
+                        Console.WriteLine("Added message to batch: {0}", message);
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                batch.Publish();
+
+                Console.WriteLine("Published batch of messages to queue '{0}'", queue);
+            }
+        }
+
+        public int MessageCount(string queue, bool durable = false)
+        {
+            using (var channel = connection.CreateModel())
+            {
+                //声明队列
+                channel.QueueDeclare(
+                queue: queue,
+                durable: durable,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+                );
+
+                // 获取消息数量
+                uint messageCount = channel.MessageCount(queue);
+                return (int)messageCount;
+            }
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+}
---
+++ WebApplication1/rabbitmq/RabbitMqSubscriber.cs
@@ -0,0 +1,216 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace WEBAPI.Core
+{
+    /// <summary>
+    /// RabbitMQ消息消费者
+    /// </summary>
+    public class RabbitMQSubscriber
+    {
+        private string exchangeName;
+        private string queueName;
+        private readonly IConnection connection;
+        //private readonly IModel channel;
+
+        /// <summary>
+        /// 初始化消费者
+        /// </summary>
+        /// <param name="uri">消息服务器地址</param>
+        /// <param name="queueName">队列名</param>
+        /// <param name="userName">用户</param>
+        /// <param name="password">密码</param>
+        /// <param name="exchangeName">交换机,有值表示广播模式</param>
+        public RabbitMQSubscriber(string uri = "amqp://localhost:5672", string userName = "guest", string password = "guest", string exchangeName = "", string queue = "")
+        {
+            var factory = new ConnectionFactory() { Uri = new Uri(uri) };
+            if (!string.IsNullOrWhiteSpace(exchangeName))
+                this.exchangeName = exchangeName;
+            if (!string.IsNullOrWhiteSpace(userName))
+                factory.UserName = userName;
+            if (!string.IsNullOrWhiteSpace(password))
+                factory.Password = password;
+            if (!string.IsNullOrWhiteSpace(queue))
+                queueName = queue;
+            try
+            {
+                connection = factory.CreateConnection();
+                //this.channel = connection.CreateModel();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /// <summary>
+        ///  触发消费行为
+        /// </summary>
+        public void Subscribe<TMessage>(string queue, Func<TMessage, bool> callback = null)
+        {
+            Subscribe(null, queue, callback);
+        }
+        /// <summary>
+        ///  触发消费行为
+        /// </summary>
+        /// <param name="queue">队列名称</param>
+        /// <param name="callback">回调方法，以及是否确认消息已被消费</param>
+        public void Subscribe<TMessage>(string exchange, string queue, Func<TMessage, bool> callback = null)
+        {
+            if (!string.IsNullOrWhiteSpace(exchange))
+                exchangeName = exchange;
+
+            // 使用自定义的队队
+            if (!string.IsNullOrWhiteSpace(queue))
+                queueName = queue;
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchangeName, "topic");//广播
+                channel.QueueDeclare(
+                    queue: queueName,
+                    durable: true,//持久化
+                    exclusive: false, //独占,只能被一个consumer使用
+                    autoDelete: false,//自己删除,在最后一个consumer完成后删除它
+                    arguments: null);
+                channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: queueName);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, e) =>
+                {
+                    var json = Encoding.UTF8.GetString(e.Body.ToArray());
+                    var result = callback(JsonHelper.DeserializeDefaultObject<TMessage>(json));
+                    if (result)
+                        channel.BasicAck(e.DeliveryTag, multiple: false);//手动确认消息已被消费
+                };
+                // 启动消费者
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: false,// 禁用自动消息确认，需要手动确认消息
+                    consumer: consumer);// 消费者对象
+                queueName = null;
+                Console.WriteLine(" [*] Waiting for messages." + "To exit press CTRL+C");
+            }
+        }
+
+        /// <summary>
+        /// 批量消费消息
+        /// </summary>
+        /// <typeparam name="TMessage">消息类型</typeparam>
+        /// <param name="queue">队列名</param>
+        /// <param name="batchSize">批量大小</param>
+        /// <param name="callback">处理批量消息的回调函数</param>
+        /// <param name="durable">持久化，需要与生产者一致</param>
+        public void ConsumeBatch<TMessage>(string queue, int batchSize, Action<List<TMessage>> callback)
+        {
+            queueName = queue;
+
+            using (var channel = connection.CreateModel())
+            {
+                // 声明队列
+                channel.QueueDeclare(
+                queue: queueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+                );
+
+                var messages = new List<TMessage>();
+
+                // 批量获取消息
+                for (int i = 0; i < batchSize; i++)
+                {
+                    BasicGetResult result = channel.BasicGet(queue: queueName, autoAck: true);
+
+                    if (result == null)
+                        continue;
+
+                    var json = Encoding.UTF8.GetString(result.Body.ToArray());
+                    var message = JsonHelper.DeserializeDefaultObject<TMessage>(json);
+                    if (message == null)
+                        Console.WriteLine(json);
+                    else
+                        messages.Add(message);
+                }
+
+                if (messages.Count > 0)
+                {
+                    // 处理批量消息
+                    callback(messages);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 广播模式
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="exchange"></param>
+        /// <param name="callback"></param>
+        public void SubscribeFanout<TMessage>(string exchange, Action<TMessage> callback = null)
+        {
+            if (!string.IsNullOrWhiteSpace(exchange))
+                exchangeName = exchange;
+
+            using (var channel = connection.CreateModel())
+            {
+                //广播模式
+                channel.ExchangeDeclare(this.exchangeName, "fanout");//广播
+                QueueDeclareOk queueOk = channel.QueueDeclare();//每当Consumer连接时，我们需要一个新的，空的queue,如果在声明queue时不指定,那么RabbitMQ会随机为我们选择这个名字
+                queueName = queueOk.QueueName;//得到RabbitMQ帮我们取了名字
+                channel.QueueBind(queueName, exchangeName, string.Empty);//不需要指定routing key，设置了fanout,指了也没有用.
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, e) =>
+                {
+                    var json = Encoding.UTF8.GetString(e.Body.ToArray());
+                    callback(JsonHelper.DeserializeDefaultObject<TMessage>(json));
+
+                    channel.BasicAck(e.DeliveryTag, multiple: false);//手动确认消息已被消费
+                };
+                // 启动消费者
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: false,
+                    consumer: consumer);
+                queueName = null;
+                Console.WriteLine(" [*] Waiting for messages." + "To exit press CTRL+C");
+                Console.ReadKey();
+            }
+        }
+
+        public string PeekMessage(string queue)
+        {
+            using (var channel = connection.CreateModel())
+            {
+                // 声明队列
+                channel.QueueDeclare(
+                queue: queue,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+                );
+
+                // 获取消息
+                BasicGetResult result = channel.BasicGet(queue: queue, autoAck: false);
+                if (result == null)
+                    return null;
+                var json = Encoding.UTF8.GetString(result.Body.ToArray());
+                return json;
+            }
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+}
