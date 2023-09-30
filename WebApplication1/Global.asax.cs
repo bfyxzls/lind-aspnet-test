20b65f124770210582d41e8247371a410cfe6e4b
lind <bfyxzls@sina.com>
2023-09-30 15:22:38 +0800

    add web project

---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using ConsoleApp1;
+using WEBAPI.Core;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Task.Run(() =>
+            {
+                RabbitMQSubscriber rabbitMqSubscriber = new RabbitMQSubscriber();
+                rabbitMqSubscriber.SubscribeFanout<UserInfo>("test_fanout", msg =>
+                {
+                    Console.WriteLine(msg.name);
+                });
+            });
+            
+        }
+    }
+}
